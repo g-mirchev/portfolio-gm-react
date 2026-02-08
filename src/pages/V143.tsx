ddb804febefbbc97407175b15ec2095751edcf85
Georgi Mirchev <kinggmirchev@gmail.com>
2026-02-08 14:13:21 +0000

    feat: mount V

---
+++ src/pages/V143.tsx
@@ -0,0 +1,13 @@
+import { type FC, type ReactElement } from 'react';
+import UnderConstruction from 'components/UnderConstruction';
+
+const V143: FC = (): ReactElement => {
+  return (
+    <UnderConstruction
+      title="Hmmm... Something's cooking!"
+      message="Stick around for a sweet surprise. Check back soon!"
+    />
+  );
+};
+
+export default V143;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,6 +5,7 @@ import Experience from '../pages/Experience';
 import Projects from '../pages/Projects';
 import Cat from '../pages/Cat';
 import TicTacToe from '../pages/TicTacToe';
+import V143 from '../pages/V143';
 import type { FC, ReactElement } from 'react';
 
 const Main: FC = (): ReactElement => {
@@ -16,6 +17,7 @@ const Main: FC = (): ReactElement => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/cat" element={<Cat />} />
         <Route path="/tic-tac-toe" element={<TicTacToe />} />
+        <Route path="/v143" element={<V143 />} />
       </Route>
     </Routes>
   )
